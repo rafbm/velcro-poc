d5a3c40a4df10d749da09722c5c8279cb457d706
Rafaël Blais Masson <rafbmasson@gmail.com>
2012-09-26 15:23:41 -0400

    Big update

--- app.rb
+++ app.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 LANGUAGES = [
   OpenStruct.new(
     code: 'fr',
@@ -28,11 +30,35 @@ LANGUAGES = [
   ),
 ]
 
+before do
+  puts "#{request.request_method} #{request.path} -- #{request.user_agent}"
+
+  headers 'Cache-Control' => 'no-cache, no-store, max-age=0, must-revalidate'
+  headers 'Pragma' => 'no-cache'
+  headers 'Expires' => 'Fri, 01 Jan 1990 00:00:00 GMT'
+end
+
 get '/' do
+  @title = 'Home'
+  @meta_description = 'Home'
   erb :index
 end
 
 get '/learn/:lang' do
   @lang = LANGUAGES.detect { |lang| lang.code == params[:lang] }
+  @title = "Learn #{@lang.name}"
+  @meta_description = "Learn #{@lang.name}"
   erb :index
+end
+
+get '/search' do
+  @title = "Search for “#{params[:q]}”"
+  @meta_description = "Search for “#{params[:q]}”"
+  erb :search
+end
+
+post '/search' do
+  @title = "Search for “#{params[:q]}”"
+  @meta_description = "Search for “#{params[:q]}”"
+  erb :search
 end
--- config.ru
+++ config.ru
@@ -1,3 +1,4 @@
+require 'erubis'
 require 'sinatra'
 require 'sprockets'
 require 'coffee-script'
